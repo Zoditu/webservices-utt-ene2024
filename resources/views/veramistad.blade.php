ddf8e77a9cf5decfac4814c81336ca7d4986077a
Anahi030702 <holguinanahi959@gmail.com>
2024-04-14 11:16:02 -0600

    cambios

--- resources/views/veramistad.blade.php
+++ resources/views/veramistad.blade.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AMISTAD</title>
+
 </head>
 <body>
+
+
     <h1>Eres amigo de {{$usu2}} desde el {{date('d-m-Y', strtotime($fecha))}}</h1>
 
     <form action="{{ route('amigos.busqueda') }}" method="get">
