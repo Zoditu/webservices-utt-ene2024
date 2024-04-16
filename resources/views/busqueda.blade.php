f241e1f54ac65a4dc3673d8a47d7c9fb7ce86b0b
WilbertACDX <115293547+WilbertACDX@users.noreply.github.com>
2024-04-16 17:04:02 -0600

    vista
    vista

--- resources/views/busqueda.blade.php
+++ resources/views/busqueda.blade.php
@@ -3,12 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Resultado de Búsqueda</title>
 </head>
 <body>
-<form action="/busqueda" method="GET">
-    <input type="text" name="q" placeholder="Buscar...">
-    <button type="submit">Buscar</button>
-</form>
+    <h1>Resultado de Búsqueda</h1>
+
+    <form action="{{ url('/busqueda') }}" method="GET">
+        <input type="text" name="q" placeholder="Buscar...">
+        <button type="submit">Buscar</button>
+    </form>
+
+    <h2>Resultados</h2>
+    @if (count($resultados) > 0)
+        <ul>
+            @foreach ($resultados as $resultado)
+                <li>{{ $resultado->name }} {{ $resultado->lastName }}: {{ $resultado->Contenido }}</li>
+            @endforeach
+        </ul>
+    @else
+        <p>No se encontraron resultados.</p>
+    @endif
 </body>
 </html>
